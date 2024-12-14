d1d8a11f4e6f11ff153b985cb87120d0cb638253
Ivan Bankov <yiwan.bankov@gmail.com>
2024-12-14 23:49:03 +0300

    create telegram bot

--- telegram/run.py
+++ telegram/run.py
@@ -1,18 +1,46 @@
 import asyncio
+import logging
 
-from aiogram import Bot, Dispatcher
-from aiogram.filters import CommandStart
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 
-
-bot = Bot(token='123')
-dp = Dispatcher
+from config import TOKEN
 
 
+bot = Bot(token= TOKEN)
+dp = Dispatcher()
 
-@dp.massage(CommandStart())
+
+
+@dp.message(CommandStart())
 async def cmd_start(message: Message):
-    await message.answer("Привет!")
+    await message.reply(f"Привет! \nтвой ID: {message.from_user.id}\nИмя: {message.from_user.first_name}")
+
+
+
+@dp.message(Command('help'))
+async def get_help(message: Message):
+    await message.answer("Это команда /help")
+
+
+
+@dp.message(F.text == 'Как дела?')
+async def how_are_you(message: Message):
+    await message.answer("ОК!")
+
+
+
+@dp.message(Command('get_photo'))
+async def get_photo(message: Message):
+    await message.answer_photo(photo= 'AgACAgIAAxkBAAMhZ13sE_d6-7XmWc5BdWoGRpzb3TgAAsPsMRsEG_FKc3Fe1s2yo4MBAAMCAAN5AAM2BA',
+                               caption= 'это твое фото')
+
+
+
+@dp.message(F.photo)
+async def get_photo(message: Message):
+    await message.answer(F"ID фото: {message.photo[-1].file_id}")
 
 
 
@@ -22,4 +50,8 @@ async def main():
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    logging.basicConfig(level=logging.INFO)
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Exit')
