ddb59e0893668228284dc13533b96c532ad862f6
Ivan Bankov <yiwan.bankov@gmail.com>
2024-12-16 03:06:16 +0300

    added database

--- telegram/app/handlers.py
+++ telegram/app/handlers.py
@@ -15,8 +15,9 @@ router.message.outer_middleware(TestMiddeware())
 
 
 
-class Reg(StatesGroup):
+class Register(StatesGroup):
     name = State()
+    grade= State()
     number = State()
 
 
@@ -24,64 +25,113 @@ class Reg(StatesGroup):
 @router.message(CommandStart())
 async def cmd_start(message: Message):
     await message.reply(f"Привет! \nтвой ID: {message.from_user.id}\nИмя: {message.from_user.first_name}",
-                        reply_markup= kb.settings)
+                        reply_markup= kb.main)
 
 
 @router.message(Command('help'))
 async def get_help(message: Message):
-    await message.answer("Это команда /help")
+    await message.answer("Мне самому нужно помощь")
 
 
-@router.message(F.text == 'Как дела?')
-async def how_are_you(message: Message):
-    await message.answer("ОК!")
+# @router.message(Command('schedule'))
+# async def schedule(message: Message):
+#     await message.answer("Расписание пока в разработке", reply_markup= kb.edit_schedule)
 
 
-@router.message(Command('get_photo'))
-async def get_photo(message: Message):
-    await message.answer_photo(photo= 'AgACAgIAAxkBAAMhZ13sE_d6-7XmWc5BdWoGRpzb3TgAAsPsMRsEG_FKc3Fe1s2yo4MBAAMCAAN5AAM2BA',
-                               caption= 'это твое фото')
+# @router.message(Command('get_photo'))
+# async def get_photo(message: Message):
+#     await message.answer_photo(photo= 'AgACAgIAAxkBAAMhZ13sE_d6-7XmWc5BdWoGRpzb3TgAAsPsMRsEG_FKc3Fe1s2yo4MBAAMCAAN5AAM2BA',
+#                                caption= 'это твое фото')
 
 
-@router.message(F.photo)
-async def get_photo(message: Message):
-    await message.answer(F"ID фото: {message.photo[-1].file_id}")
+# @router.message(F.photo)
+# async def get_photo(message: Message):
+#     await message.answer(F"ID фото: {message.photo[-1].file_id}")
 
 
-@router.callback_query(F.data == 'catalog')
-async def catalog(callback: CallbackQuery):
-    await callback.answer('Вы выбрали каталог')
-    await callback.message.answer('Привет!')
+# @router.callback_query(F.data == 'catalog')
+# async def catalog(callback: CallbackQuery):
+#     await callback.answer('Вы выбрали каталог')
+#     await callback.message.answer('Привет!')
 
 
-@router.callback_query(F.data == 'basket')
-async def basket(callback: CallbackQuery):
-    await callback.answer('Вы выбрали корзину', show_alert= True)
-    await callback.message.answer('Корзина')
+# @router.callback_query(F.data == 'basket')
+# async def basket(callback: CallbackQuery):
+#     await callback.answer('Вы выбрали корзину', show_alert= True)
+#     await callback.message.answer('Корзина')
 
 
-@router.callback_query(F.data == 'cars')
-async def cars(callback: CallbackQuery):
-    await callback.answer('Нажмите на вашу марку машины')
-    await callback.message.edit_text('Какая у тебя машина?', reply_markup= await kb.inline_cars())
+# @router.callback_query(F.data == 'cars')
+# async def cars(callback: CallbackQuery):
+#     await callback.answer('Нажмите на вашу марку машины')
+#     await callback.message.edit_text('Какая у тебя машина?', reply_markup= await kb.inline_cars())
 
 
-@router.message(Command('reg'))
-async def reg_first(message: Message, state: FSMContext):
-    await state.set_state(Reg.name)
-    await message.answer('Введите Ваше имя')
+# @router.message(Command('reg'))
+# async def reg_first(message: Message, state: FSMContext):
+#     await state.set_state(Reg.name)
+#     await message.answer('Введите Ваше имя')
 
 
-@router.message(Reg.name)
-async def reg_second(message: Message, state: FSMContext):
-    await state.update_data(name = message.text)
-    await state.set_state(Reg.number)
-    await message.answer('Введите номер телефона')
+# @router.message(Reg.name)
+# async def reg_second(message: Message, state: FSMContext):
+#     await state.update_data(name = message.text)
+#     await state.set_state(Reg.number)
+#     await message.answer('Введите номер телефона')
 
 
-@router.message(Reg.number)
-async def two_three(message: Message, state: FSMContext):
-    await state.update_data(number = message.text)
+# @router.message(Reg.number)
+# async def two_three(message: Message, state: FSMContext):
+#     await state.update_data(number = message.text)
+#     data = await state.get_data()
+#     await message.answer(f'Спасибо, регистрация завершена.\nИмя: {data["name"]}\nНомер: {data["number"]}')
+#     await state.clear()
+
+
+@router.message(F.text == 'Рассписание')
+async def schedule(message: Message):
+    await message.answer('Рассписание пока в разработке', reply_markup= kb.edit_schedule)
+
+
+@router.callback_query(F.data == 'edit')
+async def edit(callback: CallbackQuery):
+    await callback.answer('Расписание пока нельзя изменить', show_alert= True)
+    await callback.message.edit_text('Расписание пока в разработке')
+
+
+@router.message(F.text == 'Близжайшее занятие')
+async def next_lesson(message: Message):
+    await message.answer('К сожалению рассписания пока нет')
+
+
+@router.message(F.text == 'Дз')
+async def home_work(message: Message):
+    await message.answer('Данная функция в разработке')
+
+
+@router.message(Command('register'))
+async def register(message: Message, state: FSMContext):
+    await state.set_state(Register.name)
+    await message.answer('Введите ваше имя')
+
+
+@router.message(Register.name)
+async def register_name(message: Message, state: FSMContext):
+    await state.update_data(name= message.text)
+    await state.set_state(Register.grade)
+    await message.answer('Напишите в каком вы классе')
+
+
+@router.message(Register.grade)
+async def register_grade(message: Message, state: FSMContext):
+    await state.update_data(grade= message.text)
+    await state.set_state(Register.number)
+    await message.answer('Отправьте ваш номер телефона', reply_markup= kb.get_number)
+
+
+@router.message(Register.number, F.contact)
+async def register_number(message: Message, state: FSMContext):
+    await state.update_data(number= message.contact.phone_number)
     data = await state.get_data()
-    await message.answer(f'Спасибо, регистрация завершена.\nИмя: {data["name"]}\nНомер: {data["number"]}')
+    await message.answer(f'Ваше имя: {data["name"]}\nВаш класс: {data["grade"]}\nНомер: {data["number"]}')
     await state.clear()
--- telegram/app/keyboards.py
+++ telegram/app/keyboards.py
@@ -1,28 +1,39 @@
 from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton,
-                           InlineKeyboardMarkup, InlineKeyboardButton)
+                            InlineKeyboardMarkup, InlineKeyboardButton)
 from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
 
 
 main = ReplyKeyboardMarkup(keyboard= [
-    [KeyboardButton(text = 'Каталог')],
-    [KeyboardButton(text= 'Коризна'), KeyboardButton(text= 'Контакты')]
+    [KeyboardButton(text = 'Рассписание')],
+    [KeyboardButton(text= 'Близжайшее занятие'), KeyboardButton(text= 'Дз')]
 ],
                             resize_keyboard= True,
                             input_field_placeholder= 'Выберете пункт меню')
 
 
-settings = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text= 'YouTube', url= 'https://www.youtube.com/watch?v=dQw4w9WgXcQ')],
-    [InlineKeyboardButton(text= 'Каталог', callback_data= 'catalog')],
-    [InlineKeyboardButton(text= 'Корзина', callback_data= 'basket')],
-    [InlineKeyboardButton(text= 'Какая у тебя машина?', callback_data= 'cars'),]
+# settings = InlineKeyboardMarkup(inline_keyboard=[
+#     [InlineKeyboardButton(text= 'YouTube', url= 'https://www.youtube.com/watch?v=dQw4w9WgXcQ')],
+#     [InlineKeyboardButton(text= 'Каталог', callback_data= 'catalog')],
+#     [InlineKeyboardButton(text= 'Корзина', callback_data= 'basket')],
+#     [InlineKeyboardButton(text= 'Какая у тебя машина?', callback_data= 'cars'),]
+#     ])
+
+
+# cars = ['Tesla', 'Mercedes', 'BMW', 'Toyota']
+
+# async def inline_cars():
+#     keyboard = InlineKeyboardBuilder()
+#     for car in cars:
+#         keyboard.add(InlineKeyboardButton(text=car, url= 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'))
+#     return keyboard.adjust(2).as_markup()
+
+edit_schedule = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text= 'Изменить расписание', callback_data= 'edit')]
     ])
 
 
-cars = ['Tesla', 'Mercedes', 'BMW', 'Toyota']
-
-async def inline_cars():
-    keyboard = InlineKeyboardBuilder()
-    for car in cars:
-        keyboard.add(InlineKeyboardButton(text=car, url= 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'))
-    return keyboard.adjust(2).as_markup()
+get_number = ReplyKeyboardMarkup(keyboard=[
+    [KeyboardButton(text= 'Отправить номер', request_contact= True)]
+], 
+                                resize_keyboard= True,
+                                input_field_placeholder= 'Дать номер телефона')
